68205fbbea511cab3394ec2740a242a811c8e116
likai <lkmjbj@163.com>
2019-07-04 12:29:35 +0800

    修改模糊查询

--- src/test/java/com/ydsh/finance/ServiceDemoApplicationTests.java
+++ src/test/java/com/ydsh/finance/ServiceDemoApplicationTests.java
@@ -16,6 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -60,7 +64,51 @@ public class ServiceDemoApplicationTests {
 //        }
 //        s.saveOrUpdateBatch(orders);
 //    }
+@Test
+public void forMap(){
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("xxx", "000");
+    map.put("aaa", "111");
+    map.put("bbb", "222");
+    map.put("ccc", "333");
+    map.put("ddd", "444");
+    map.put("eee", "555");
 
+    Iterator<Map.Entry<String, String>> iterator  = map.entrySet().iterator();
+
+    while (iterator.hasNext()) {
+
+        Map.Entry<String, String> entry = iterator.next();
+
+        System.out.println(entry.getKey()+":"+entry.getValue());
+
+    }
+
+}
+    @Test
+    public void t1(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("xxx", "000");
+        map.put("aaa", "111");
+        map.put("bbb", 2);
+        map.put("ccc", "333");
+        map.put("ddd", "444");
+        map.put("eee", 3);
+        System.out.println(map);
+
+        //map的遍历
+        for(Map.Entry<String, Object> entry:map.entrySet()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+            if(entry.getValue() instanceof String){
+                System.out.println("Value是String类型");
+            }
+            if(entry.getValue() instanceof Integer){
+                System.out.println("Value是Integer类型");
+            }
+
+        }
+
+    }
     public static void main(String[] args) {
         Result<String> result=new Result<String>(0,"success","");
         System.out.println(JSON.toJSONString(result));
